426b3ae4e991c060c1a748bf36c34d3f4e102c7f
RameshMamidipelly <rameshm060695@gmail.com>
2020-04-07 22:52:09 +0530

    praneeth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShirtsComponent } from './mens-wear/shirts/shirts.component';
+import { TShirtsComponent } from './mens-wear/t-shirts/t-shirts.component';
+import { ShoesComponent } from './mens-wear/shoes/shoes.component';
+import { DressesComponent } from './ladies/dresses/dresses.component';
+import { HeaderMobilesComponent } from './mobiles/header-mobiles/header-mobiles.component';
+import { HeaderMensComponent } from './mens-wear/header-mens/header-mens.component';
+import { HeaderLadiesComponent } from './ladies/header-ladies/header-ladies.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"shirts",component:ShirtsComponent
+},
+{
+  path:'t-shirts',component:TShirtsComponent
+},
+{
+  path:'shoes',component:ShoesComponent
+},
+{
+  path:'dresses',component:DressesComponent
+},
+{
+  path:'mobiles',component:HeaderMobilesComponent
+}
+,
+{
+  path:'means-wear',component:HeaderMensComponent
+}
+,
+{
+  path:'ladies-wear',component:HeaderLadiesComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftNavigationComponent } from './modules/left-navigation/left-navigation.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { FooterComponent } from './footer/footer.component';
+import { SamsungComponent } from './mobiles/samsung/samsung.component';
+import { MiComponent } from './mobiles/mi/mi.component';
+import { LenovoComponent } from './mobiles/lenovo/lenovo.component';
+import { ShirtsComponent } from './mens-wear/shirts/shirts.component';
+import { TShirtsComponent } from './mens-wear/t-shirts/t-shirts.component';
+import { ShoesComponent } from './mens-wear/shoes/shoes.component';
+import { DressesComponent } from './ladies/dresses/dresses.component';
+import { HeaderMobilesComponent } from './mobiles/header-mobiles/header-mobiles.component';
+import { HeaderLadiesComponent } from './ladies/header-ladies/header-ladies.component';
+import { HeaderMensComponent } from './mens-wear/header-mens/header-mens.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftNavigationComponent,
+    SlideshowComponent,
+    FooterComponent,
+    SamsungComponent,
+    MiComponent,
+    LenovoComponent,
+    ShirtsComponent,
+    TShirtsComponent,
+    ShoesComponent,
+    DressesComponent,
+    HeaderMobilesComponent,
+    HeaderLadiesComponent,
+    HeaderMensComponent
   ],
   imports: [
     BrowserModule,
